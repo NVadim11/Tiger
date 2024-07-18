ce39e51cb4b4faf4910885b3ce4d6b114c3b950a
NVadim <palaginvad@gmail.com>
2024-07-18 12:33:50 +0300

    sync

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -716,14 +716,36 @@ const Footer = ({ user }) => {
 														</span>
 														{quest.status === 0 ? (
 															<p className='popupTasks__task-rew'>
-															<span style={{alignItems:'center', background: '#2cb726', border: '1px solid #0d9047', borderRadius: '31px', color: '#ffffff', display: 'flex', justifyContent: 'center', padding: '6px 12px', width: '65%' }}>{t('activityClaim')}</span>
+																<span
+																	style={{
+																		alignItems: 'center',
+																		background: '#2cb726',
+																		border: '1px solid #0d9047',
+																		borderRadius: '31px',
+																		color: '#ffffff',
+																		display: 'flex',
+																		justifyContent: 'center',
+																		padding: '6px 12px',
+																		width: '65%',
+																	}}
+																>
+																	{t('activityClaim')}
+																</span>
 																{quest.reward}{' '}
 																<div className='rewardCoin'>
 																	<img src={tigerCoin} alt='Tiger coin' />
 																</div>
 															</p>
 														) : (
-															<span style={{ width: 'auto', position: 'absolute', right: '35px' }}>{t('activityDone')}</span>
+															<span
+																style={{
+																	width: 'auto',
+																	position: 'absolute',
+																	right: '35px',
+																}}
+															>
+																{t('activityDone')}
+															</span>
 														)}
 													</button>
 												) : (
@@ -748,7 +770,15 @@ const Footer = ({ user }) => {
 																</div>
 															</p>
 														) : (
-															<span style={{ width: 'auto', position: 'absolute', right: '35px' }}>{t('activityDone')}</span>
+															<span
+																style={{
+																	width: 'auto',
+																	position: 'absolute',
+																	right: '35px',
+																}}
+															>
+																{t('activityDone')}
+															</span>
 														)}
 													</button>
 												)}
@@ -792,25 +822,15 @@ const Footer = ({ user }) => {
 																</div>
 															</p>
 														) : (
-															<svg
-																width='24'
-																height='24'
-																viewBox='0 0 24 24'
-																fill='none'
-																xmlns='http://www.w3.org/2000/svg'
+															<span
+																style={{
+																	width: 'auto',
+																	position: 'absolute',
+																	right: '35px',
+																}}
 															>
-																<g clipPath='url(#clip0_438_3053)'>
-																	<path
-																		d='M12 0C5.38346 0 0 5.38346 0 12C0 18.6165 5.38346 24 12 24C18.6165 24 24 18.6165 24 12C24 5.38346 18.6165 0 12 0ZM18.7068 8.84211L11.0376 16.4511C10.5865 16.9023 9.86466 16.9323 9.38346 16.4812L5.32331 12.782C4.84211 12.3308 4.81203 11.5789 5.23308 11.0977C5.68421 10.6165 6.43609 10.5865 6.91729 11.0376L10.1353 13.985L16.9925 7.12782C17.4737 6.64662 18.2256 6.64662 18.7068 7.12782C19.188 7.60902 19.188 8.3609 18.7068 8.84211Z'
-																		fill='#2CB726'
-																	/>
-																</g>
-																<defs>
-																	<clipPath id='clip0_438_3053'>
-																		<rect width='24' height='24' fill='white' />
-																	</clipPath>
-																</defs>
-															</svg>
+																{t('activityDone')}
+															</span>
 														)}
 													</button>
 												</div>
